76e169d9525c2604b4c18775731937e9c4e3791a
Dwika <dwikariswanda@gmail.com>
2022-09-19 03:53:59 +0700

    Chapter 4 Topic 3 (Live Data): Implement activity intent

--- app/src/main/java/com/example/chapter4_topic3_livedata/DetailActivity.kt
+++ app/src/main/java/com/example/chapter4_topic3_livedata/DetailActivity.kt
@@ -1,33 +1,22 @@
-package com.example.chapter4_topic3_livedata.fragments
+package com.example.chapter4_topic3_livedata
 
 import android.content.Intent
-import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
-import androidx.fragment.app.Fragment
+import androidx.databinding.DataBindingUtil
 import com.bumptech.glide.Glide
-import com.example.chapter4_topic3_livedata.R
 import com.example.chapter4_topic3_livedata.data.Product
-import com.example.chapter4_topic3_livedata.databinding.FragmentDetailBinding
+import com.example.chapter4_topic3_livedata.databinding.ActivityDetailBinding
 
-
-class DetailFragment : Fragment() {
-
-    private lateinit var binding: FragmentDetailBinding
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        binding = FragmentDetailBinding.inflate(layoutInflater, container, false)
-        return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        val product = arguments?.getParcelable<Product>("product_details") as Product
+class DetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_detail)
+        setContentView(binding.root)
+        val product = intent.getParcelableExtra<Product>("product_details") as Product
         binding.product = product
         binding.btnOrderNow.setOnClickListener {
             val sendIntent = Intent()
@@ -39,7 +28,6 @@ class DetailFragment : Fragment() {
             startActivity(sendIntent)
         }
     }
-
     companion object{
         @JvmStatic
         @BindingAdapter("productImage")
@@ -50,5 +38,4 @@ class DetailFragment : Fragment() {
                 .into(view)
         }
     }
-
 }
--- app/src/main/java/com/example/chapter4_topic3_livedata/MainActivity.kt
+++ app/src/main/java/com/example/chapter4_topic3_livedata/MainActivity.kt
@@ -1,15 +1,88 @@
 package com.example.chapter4_topic3_livedata
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.chapter4_topic3_livedata.fragments.HomeFragment
+import android.util.Log
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.viewpager2.widget.MarginPageTransformer
+import androidx.viewpager2.widget.ViewPager2
+import com.example.chapter4_topic3_livedata.adapter.CategoryAdapter
+import com.example.chapter4_topic3_livedata.adapter.HighlightAdapter
+import com.example.chapter4_topic3_livedata.adapter.ProductItemAdapter
+import com.example.chapter4_topic3_livedata.data.Product
+import com.example.chapter4_topic3_livedata.databinding.ActivityMainBinding
+import com.example.chapter4_topic3_livedata.viewmodel.ProductViewModel
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var productVM: ProductViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.frContainer, HomeFragment())
-            .commit()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        productVM = ViewModelProvider(this)[ProductViewModel::class.java]
+        setViews()
+    }
+    fun setViews(){
+        val categoryList = arrayListOf<String>(
+            "Male",
+            "Women",
+            "Summer",
+            "Winter",
+        )
+        val highlightAdapter = HighlightAdapter(ArrayList())
+        binding.vpHighlight.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+        binding.vpHighlight.offscreenPageLimit = 3
+        binding.vpHighlight.adapter = highlightAdapter
+        binding.vpHighlight.setPageTransformer(MarginPageTransformer(50))
+        binding.vpHighlight.clipToPadding = false
+        binding.vpHighlight.setPadding(10,10,10,0)
+        highlightAdapter.setOnItemClickListener(object : HighlightAdapter.OnItemClickListener{
+            override fun onItemClick(id: Int) {
+                moveToDetail(id)
+            }
+        })
+
+        val categoryAdapter = CategoryAdapter(categoryList)
+        binding.rvCategory.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+        binding.rvCategory.adapter = categoryAdapter
+        categoryAdapter.setOnItemClickListener(object : CategoryAdapter.OnItemClickListener{
+            override fun onItemClick(category: String) {
+                Log.d("Category Adapter", "onItemClick: $category")
+            }
+        })
+
+        val productAdapter = ProductItemAdapter(ArrayList())
+        binding.rvProducts.layoutManager = object : GridLayoutManager(this, 2, GridLayoutManager.VERTICAL, false){
+            override fun canScrollVertically(): Boolean {
+                return false
+            }
+        }
+        productVM.setProductLiveData()
+        productVM.productListLiveData.observe(this, Observer {
+            highlightAdapter.setHighlightList(it as ArrayList<Product>)
+            productAdapter.setProductList(it as ArrayList<Product>)
+        })
+        binding.rvProducts.adapter = productAdapter
+        productAdapter.setOnItemClickListener(object: ProductItemAdapter.OnItemClickListener{
+            override fun onItemClick(id: Int) {
+                moveToDetail(id)
+            }
+        })
+    }
+
+    fun moveToDetail(id: Int){
+        val bundle = Bundle()
+        val product = productVM.productListLiveData.value?.first {
+            it.id == id
+        } as Product
+        bundle.putParcelable("product_details", product)
+        val intent = Intent(this@MainActivity, DetailActivity::class.java)
+        intent.putExtras(bundle)
+        startActivity(intent)
     }
 }
